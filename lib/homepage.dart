4518b7ec237af4f7328b2e2113ff67013d3bb34f
unknown <platonn250@gmail.com>
2022-08-03 15:22:42 +0200

    h

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,10 +1,5 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, depend_on_referenced_packages
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:crudfire/firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:crudfire/homepage.dart';
-import 'package:flutter/material.dart';
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
